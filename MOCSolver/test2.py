458a276a3b864f543bebcba05bd5a82fa104c939
Alicia M. Elliott <aliciae@mit.edu>
2017-10-11 11:20:06 -0400

    flux converges in 2 iterations!

--- MOCSolver/test2.py
+++ MOCSolver/test2.py
@@ -4,14 +4,14 @@ from flatsource import FlatSourceApproximation
 from flux import MOCFlux
 from convergence import ConvergenceTest
 
-num_azim = 8 #number of azimuthal angles desired
-t = 0.1#track spacing desired, cm
+num_azim = 16 #number of azimuthal angles desired
+t = 0.05#track spacing desired, cm
 h = 1.26 #height of pincell
 w = 1.26 #width of pincell
 r = 0.4 #fuel pin radius
-n_p = 2 #number of polar divisions; can be 2 or 3
+n_p = 3 #number of polar divisions; can be 2 or 3
 num_rings = 0 #number of rings to create to divide fuel into FSRs
-q_fuel = 1 #constant isotropic source (divide by 1/4pi in later code)
+q_fuel = 1 #constant isotropic source
 q_mod = 0 #no source in moderator
 
 ndensity_fuel = 2.2e22 #atoms/cc
@@ -20,8 +20,8 @@ ndensity_mod = 1.0e21 #at/cc
 sigma_a = 1 * 1e-24
 sigma_r = (11.4 + 8)* 1e-24
 
-sigma_t_fuel = 1e5
-#sigma_t_fuel = sigma_r * ndensity_fuel
+#sigma_t_fuel = 1e5 #for computing Dancoff factors
+sigma_t_fuel = sigma_r * ndensity_fuel
 sigma_t_mod = sigma_a * ndensity_mod
 
 
@@ -55,14 +55,23 @@ psi_in1 = psi_in
 
 
 
-tol = 1e-5
+tol = 1e-7
+converged = False
+psi_old = flux.psi_scalar
+check = ConvergenceTest()
+iter = 0
+
 #for count in range(30):
 while not converged:
+    iter += 1
     flux.totalAngularFlux(psi_in1, tracks.segsource)
     flux.scalarFlux(source.sigma_FSR, tracks.segarea, tracks.omega_m, tracks.omega_p, tracks.t_eff, tracks.sintheta_p, tracks.segangle)
     psi_in1 = flux.flux_out
-    converged = ConvergenceTest.isConverged(flux.psi_scalar, psi_old, tol)
+    if iter > 1:
+        converged = check.isConverged(flux.psi_scalar, psi_old, tol)
     psi_old = flux.psi_scalar
 
+
+print iter
 #tracks.plotScalarFlux(flux.psi_scalar)
 
